461fde69fdb1fc9e2a16a2d53d57e3215611bd4e
Kurt Casey V. Naranjo <63389327+ConniverNar@users.noreply.github.com>
2024-11-09 16:35:59 +0800

    Update Month.java
    added getDaysInMonth() 
    added next()
    added previous()

--- Month.java
+++ Month.java
@@ -21,6 +21,36 @@ class Month {
         return monthNames[monthNumber - 1];
     }
 
+    public int getDaysInMonth() {
+        // Return the number of days in the month
+        switch (monthNumber) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12: // 31 days
+                return 31;
+            case 4: case 6: case 9: case 11: // 30 days
+                return 30;
+            case 2: // February (assuming non-leap year for simplicity)
+                return 28;
+            default:
+                return 30; // Default case (should not happen)
+        }
+    }
+
+    public void next() {
+        if (monthNumber == 12) {
+            monthNumber = 1; // Wrap around to January
+        } else {
+            monthNumber++;
+        }
+    }
+
+    public void previous() {
+        if (monthNumber == 1) {
+            monthNumber = 12; // Wrap around to December
+        } else {
+            monthNumber--;
+        }
+    }
+    
     @Override
     public String toString() {
         return getMonthName();
